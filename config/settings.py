1ba8a1991d5312594a949a7ae0dbc914812908ad
Nikolay <nikolay.p.95.workmail@gmail.com>
2026-01-13 02:41:46 +0500

    Добавлены Swagger/Redoc/Schema только для админов

--- config/settings.py
+++ config/settings.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
     'apps.subscriptions.apps.SubscriptionsConfig',
 
     'rest_framework',
+    'drf_spectacular',
 ]
 
 MIDDLEWARE = [
@@ -130,3 +131,51 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+
+#--------------------------------------------------------------------------------------------------------
+# Настройка API
+
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema', # схема по умолчанию
+    'DEFAULT_PERMISSION_CLASSES': ['rest_framework.permissions.IsAuthenticated',], # по умолчанию для запрет API без аутентификации
+
+    # 'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'], # подключаем фильтры
+
+    # 'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination', # подключаем пагинацию
+    # 'PAGE_SIZE': 10  # значение пагинации по умолчанию
+}
+
+
+#--------------------------------------------------------------------------------------------------------
+# Настройки spectacular для Swagger
+
+SPECTACULAR_SETTINGS = {
+    'TITLE': 'Your Project API',
+    'DESCRIPTION': 'Your project description',
+    'VERSION': '1.0.0',
+    'SERVE_INCLUDE_SCHEMA': False,
+
+    # Другие настройки
+    'SWAGGER_UI_SETTINGS': {
+        'deepLinking': True,
+        'persistAuthorization': True,
+        'displayOperationId': True,
+    },
+
+    'COMPONENT_SPLIT_REQUEST': True,
+    'SORT_OPERATIONS': False,
+
+    'SECURITY': [
+        {
+            'Bearer': [],
+        }
+    ],
+    'SECURITY_SCHEMES': {
+        'Bearer': {
+            'type': 'http',
+            'scheme': 'bearer',
+            'bearerFormat': 'JWT',
+        }
+    }
+}
